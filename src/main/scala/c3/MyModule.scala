ad048a30de5771de621ef25da7f4feae52ba5d62
Yarnspinner <arbitraryemail25@gmx.com>
2018-10-28 16:00:51 +0800

    Completed c4

--- src/main/scala/c3/MyModule.scala
+++ src/main/scala/c3/MyModule.scala
@@ -2,35 +2,35 @@ package c3
 
 import datastructures._
 object MyModule {
-  def exercise31() = List(1,2,3,4,5) match {
+  def exercise31() = MyList(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case `myNil` => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h,t) => h + List.sum(t)
+    case Cons(h,t) => h + MyList.sum(t)
     case _ => 101
   }
 
-  def exercise32() = List.tail(List(1,2,3,4,5))
+  def exercise32() = MyList.tail(MyList(1,2,3,4,5))
 
-  def exercise33() = List.setHead(100, List(1,2,3,4,5))
-  def exercise34() = List.drop(List(1,2,3,4,5), 100)
-  def exercise35() = List.dropWhile(List(1,2,3,4,5))( (x) => x <= 3)
-  def exercise36() = List.init(List(1,2,3,4,5))
+  def exercise33() = MyList.setHead(100, MyList(1,2,3,4,5))
+  def exercise34() = MyList.drop(MyList(1,2,3,4,5), 100)
+  def exercise35() = MyList.dropWhile(MyList(1,2,3,4,5))((x) => x <= 3)
+  def exercise36() = MyList.init(MyList(1,2,3,4,5))
 
-  def exercise312withFold() = List.foldLeft(List(1,2,3,4,5), myNil: List[Int])((acc, x) => Cons(x, acc))
+  def exercise312withFold() = MyList.foldLeft(MyList(1,2,3,4,5), myNil: MyList[Int])((acc, x) => Cons(x, acc))
 
-  def exercise312withoutFold() = List.reverse(List(1,2,3,4,5))
-  def exercise313() = List.foldLeftViaFoldRight(List(1,2,3,4,5), myNil: List[Int])((acc, x) => Cons(x, acc))
+  def exercise312withoutFold() = MyList.reverse(MyList(1,2,3,4,5))
+  def exercise313() = MyList.foldLeftViaFoldRight(MyList(1,2,3,4,5), myNil: MyList[Int])((acc, x) => Cons(x, acc))
 
-  def exercise321() = List.myFlatMap(List(1,2,3,4))(a =>  if (a % 2 == 0) List(a) else myNil)
+  def exercise321() = MyList.myFlatMap(MyList(1,2,3,4))(a =>  if (a % 2 == 0) MyList(a) else myNil)
 
   def exercise324() = {
-    val l1 = List(1,2,3,4)
-    val l2 = List(2,3)
-    val l3 = List(4,2)
+    val l1 = MyList(1,2,3,4)
+    val l2 = MyList(2,3)
+    val l3 = MyList(4,2)
 
-    def isSubsequence[A](sup:List[A], sub:List[A]) : Boolean = {
-      def helper(big:List[A], small:List[A]):Boolean = big match {
+    def isSubsequence[A](sup:MyList[A], sub:MyList[A]) : Boolean = {
+      def helper(big:MyList[A], small:MyList[A]):Boolean = big match {
 
         case Cons(x,xs) => small match {
           case Cons(y,ys) => if (y==x) helper(xs,ys) else helper(xs, small)
@@ -56,24 +56,24 @@ object MyModule {
     which in the case of `foldRight` means traversing the list all the way to the end. We need _non-strict_ evaluation
       to support early termination---we discuss this in chapter 5.
     */
-    println("Ex 3.8 using foldRight with constructors: %s".format(List.foldRight(List(1,2,3), myNil : List[Int])(Cons(_,_))))
-    println("Ex 3.9 using foldRight to get length: %s".format(List.length(List(1,2))))
-    println("Ex 3.10 (Writing foldleft) using foldLeft to get sum: %s".format(List.foldLeft(List(1,2, 3), 0)(_ + _)))
-    println("Ex 3.11 using foldLeft to get pdt: %s".format(List.foldLeft(List(1,2, 3), 1)(_ * _)))
-    println("Ex 3.11 using foldLeft to get Length: %s".format(List.foldLeft(List(1,2,3), 0)((acc, _) => acc + 1)))
+    println("Ex 3.8 using foldRight with constructors: %s".format(MyList.foldRight(MyList(1,2,3), myNil : MyList[Int])(Cons(_,_))))
+    println("Ex 3.9 using foldRight to get length: %s".format(MyList.length(MyList(1,2))))
+    println("Ex 3.10 (Writing foldleft) using foldLeft to get sum: %s".format(MyList.foldLeft(MyList(1,2, 3), 0)(_ + _)))
+    println("Ex 3.11 using foldLeft to get pdt: %s".format(MyList.foldLeft(MyList(1,2, 3), 1)(_ * _)))
+    println("Ex 3.11 using foldLeft to get Length: %s".format(MyList.foldLeft(MyList(1,2,3), 0)((acc, _) => acc + 1)))
     println("Ex 3.12 using foldLeft to get reverse: %s".format(exercise312withFold()))
     println("Ex 3.12 not using foldLeft to get reverse: %s".format(exercise312withoutFold()))
     println("Ex 3.13 using foldLeft to do foldRight: %s".format(exercise313()))
-    println("Ex 3.14 using foldRight to do append: %s".format(List.appendWithFoldRight(List(1,2,3), List(4))))
-    println("Ex 3.15 using appendMany: %s".format(List.appendMany(List(1,2,3), List(4), List(5,6))))
-    println("Ex 3.16 using incrementAll: %s".format(List.incrementAll(List(1,2,3))))
-    println("Ex 3.17 using allDoublesToString: %s".format(List.allDoublesToString(List(1.0,2.0,3.0))))
-    println("Ex 3.18 using divide via myMap: %s".format(List.myMap(List(1.0,2.0,3.0))((a) => a/2)))
-    println("Ex 3.19 using divide via myFilter: %s".format(List.myFilter(List(1.0,2.0,3.0))((a) => a <= 2)))
-    println("Ex 3.20 using pairs via myFlatMap: %s".format(List.myFlatMap(List(1.0,2.0,3.0))((a) => List(a,a))))
+    println("Ex 3.14 using foldRight to do append: %s".format(MyList.appendWithFoldRight(MyList(1,2,3), MyList(4))))
+    println("Ex 3.15 using appendMany: %s".format(MyList.appendMany(MyList(1,2,3), MyList(4), MyList(5,6))))
+    println("Ex 3.16 using incrementAll: %s".format(MyList.incrementAll(MyList(1,2,3))))
+    println("Ex 3.17 using allDoublesToString: %s".format(MyList.allDoublesToString(MyList(1.0,2.0,3.0))))
+    println("Ex 3.18 using divide via myMap: %s".format(MyList.myMap(MyList(1.0,2.0,3.0))((a) => a/2)))
+    println("Ex 3.19 using divide via myFilter: %s".format(MyList.myFilter(MyList(1.0,2.0,3.0))((a) => a <= 2)))
+    println("Ex 3.20 using pairs via myFlatMap: %s".format(MyList.myFlatMap(MyList(1.0,2.0,3.0))((a) => MyList(a,a))))
     println("Ex 3.21 filter via flatmap: %s".format(exercise321()))
-    println("Ex 3.22 add two list together: %s".format(List.addTwoLists(List(1,2,3), List(5, 5, 5))))
-    println("Ex 3.23 add two list together via myZipWith: %s".format(List.myZipWith(List(1,2,3), List(5, 5, 5))(_ + _)))
+    println("Ex 3.22 add two list together: %s".format(MyList.addTwoLists(MyList(1,2,3), MyList(5, 5, 5))))
+    println("Ex 3.23 add two list together via myZipWith: %s".format(MyList.myZipWith(MyList(1,2,3), MyList(5, 5, 5))(_ + _)))
 
     println("Trying out built-in list API")
     println("take first 2 elems of list: %s".format((1::2::3::Nil) take 2 ))
--- src/main/scala/datastructures/MyList.scala
+++ src/main/scala/datastructures/MyList.scala
@@ -1,38 +1,38 @@
 package datastructures
 
-sealed trait List[+A]
-case object myNil extends List[Nothing]
-case class Cons[+A](head :A, tail: List[A]) extends List[A]
+sealed trait MyList[+A]
+case object myNil extends MyList[Nothing]
+case class Cons[+A](head :A, tail: MyList[A]) extends MyList[A]
 
-object List {
-  def sum(ints: List[Int]): Int = ints match {
+object MyList {
+  def sum(ints: MyList[Int]): Int = ints match {
     case `myNil` => 0
     case Cons(x,xs) => x + sum(xs)
   }
 
-  def product(ints: List[Int]) : Int = ints match {
+  def product(ints: MyList[Int]) : Int = ints match {
     case `myNil` => 1
     case Cons(0,xs) => 0
     case Cons(x,xs) => x * product(xs)
   }
 
-  def apply[A](as : A*):List[A] =
+  def apply[A](as : A*):MyList[A] =
     if (as.isEmpty) myNil
     else Cons(as.head, apply(as.tail: _*))
 
-  def tail[A](xs : List[A]):List[A] = xs match
+  def tail[A](xs : MyList[A]):MyList[A] = xs match
   {
     case Cons(hd, tl) => tl
     case `myNil` => myNil
   }
 
-  def setHead[A](x: A, xs:List[A]) : List[A] = xs match
+  def setHead[A](x: A, xs:MyList[A]) : MyList[A] = xs match
     {
     case Cons(hd, tl) => Cons(x, tl)
     case `myNil` => Cons(x, myNil)
   }
 
-  def drop[A](l: List[A], n: Int) : List[A] =
+  def drop[A](l: MyList[A], n: Int) : MyList[A] =
     if (n == 0)
       l
     else
@@ -41,32 +41,32 @@ object List {
         case `myNil` => myNil
       }
 
-  def dropWhile[A](l:List[A])(p: A => Boolean):List[A] = l match
+  def dropWhile[A](l:MyList[A])(p: A => Boolean):MyList[A] = l match
     {
     case `myNil` => myNil
     case Cons(x,xs) => if (p(x)) dropWhile(xs)(p) else l
   }
 
-  def append[A](a1: List[A], a2: List[A]) : List[A] = a1 match {
+  def append[A](a1: MyList[A], a2: MyList[A]) : MyList[A] = a1 match {
     case `myNil` => a2
     case Cons(x,xs) => Cons(x,append(xs, a2))
   }
 
-  def reverse[A](l: List[A]) : List[A] = {
-    def helper(l : List[A], acc: List[A]) : List[A] = l match {
+  def reverse[A](l: MyList[A]) : MyList[A] = {
+    def helper(l : MyList[A], acc: MyList[A]) : MyList[A] = l match {
       case `myNil` => acc
       case Cons(x, xs) => helper(xs, Cons(x, acc))
     }
     helper(l, myNil)
   }
   @annotation.tailrec
-  def foldLeft[A,B](as: List[A], z: B)(f: (B,A) => B) : B = as match {
+  def foldLeft[A,B](as: MyList[A], z: B)(f: (B,A) => B) : B = as match {
     case `myNil` => z
     case Cons(x,xs) => foldLeft(xs, f(z, x))(f)
   }
 
 
-  def foldRight[A,B](as: List[A], z: B)(f:(A,B) => B): B =
+  def foldRight[A,B](as: MyList[A], z: B)(f:(A,B) => B): B =
     as match {
       case `myNil` => z
       case Cons(x,xs) => f(x,foldRight(xs,z)(f))
@@ -74,42 +74,42 @@ object List {
 
 //  def foldLeftViaFoldRight[A,B](as: List[A], z: B)(f: (B,A) => B) : B = foldRight(reverse(as), z)((a,b) => f(b, a))
 
-  def foldLeftViaFoldRight[A,B](l: List[A], z: B)(f: (B,A) => B): B =
+  def foldLeftViaFoldRight[A,B](l: MyList[A], z: B)(f: (B,A) => B): B =
     foldRight(l, (b:B) => b)((a,g) => b => g(f(b,a)))(z)
 
-  def sum2(ns: List[Int]) = foldRight(ns, 0)(_ + _)
+  def sum2(ns: MyList[Int]) = foldRight(ns, 0)(_ + _)
 
-  def product2(ns: List[Int]) = foldRight(ns, 1)((x,y) => x * y)
+  def product2(ns: MyList[Int]) = foldRight(ns, 1)((x, y) => x * y)
 
-  def length[A](ns:List[A]) : Int = foldRight(ns, 0)((x,y) => y + 1)
+  def length[A](ns:MyList[A]) : Int = foldRight(ns, 0)((x, y) => y + 1)
 
-  def appendWithFoldRight[A](a1 : List[A], a2 : List[A]) : List[A] = foldRight(a1,a2)((a,z) => Cons(a,z))
+  def appendWithFoldRight[A](a1 : MyList[A], a2 : MyList[A]) : MyList[A] = foldRight(a1,a2)((a, z) => Cons(a,z))
 
-  def incrementAll(xs: List[Int]): List[Int] = xs match {
+  def incrementAll(xs: MyList[Int]): MyList[Int] = xs match {
     case `myNil` => myNil
     case Cons(a, as) => Cons(a + 1, incrementAll(as))
   }
 
-  def myMap[A,B](xs : List[A])( f: (A) => B):List[B] = xs match {
+  def myMap[A,B](xs : MyList[A])(f: (A) => B):MyList[B] = xs match {
     case `myNil` => myNil
     case Cons(a,as) => Cons(f(a), myMap(as)(f))
   }
 
-  def myFilter[A](xs: List[A])(f: (A) => Boolean) : List[A] = xs match {
+  def myFilter[A](xs: MyList[A])(f: (A) => Boolean) : MyList[A] = xs match {
     case `myNil` => myNil
     case Cons(y,ys) => if (f(y)) Cons(y, myFilter(ys)(f)) else myFilter(ys)(f)
   }
 
-  def myFlatMap[A,B](as:List[A])(f : A => List[B]):List[B] = as match {
+  def myFlatMap[A,B](as:MyList[A])(f : A => MyList[B]):MyList[B] = as match {
     case `myNil` => myNil
     case Cons(y,ys) => append(f(y), myFlatMap(ys)(f))
   }
-  def allDoublesToString(xs: List[Double]) : List[String] = xs match {
+  def allDoublesToString(xs: MyList[Double]) : MyList[String] = xs match {
     case `myNil` => myNil
     case Cons(a, as) => Cons(a.toString(), allDoublesToString(as))
   }
 
-  def addTwoLists(as: List[Int], bs:List[Int]) : List[Int] = as match {
+  def addTwoLists(as: MyList[Int], bs:MyList[Int]) : MyList[Int] = as match {
     case `myNil` => myNil
     case Cons(a, aas) => bs match {
       case `myNil` => myNil
@@ -117,17 +117,17 @@ object List {
     }
   }
 
-  def myZipWith[A](as:List[A], bs: List[A])(f : (A,A) => A) : List[A] = as match {
+  def myZipWith[A](as:MyList[A], bs: MyList[A])(f : (A,A) => A) : MyList[A] = as match {
     case `myNil` => myNil
     case Cons(a, aas) => bs match {
       case `myNil` => myNil
       case Cons(b, bbs) => Cons(f(a,b), myZipWith(aas, bbs)(f))
     }
   }
-  def appendMany[A](as : List[A]*) : List[A] =
+  def appendMany[A](as : MyList[A]*) : MyList[A] =
     if (as.isEmpty) myNil
     else append(as.head, appendMany(as.tail: _*))
-  def init[A](l: List[A]) : List[A] = l match {
+  def init[A](l: MyList[A]) : MyList[A] = l match {
     case `myNil` => myNil
     case Cons(x, `myNil`) => myNil
     case Cons(x, xs) => Cons(x,init(xs))
