7e6beb7d29bfda79bebefa87f7a1e5af02415152
Yarnspinner <arbitraryemail25@gmx.com>
2018-11-01 10:36:27 +0800

    c7 partial

--- src/main/scala/datastructures/Par.scala
+++ src/main/scala/datastructures/Par.scala
@@ -1,16 +1,56 @@
 package datastructures
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit};
 
 class Par[+A]
 
 object Par {
-    def unit[A](a : A) : Par[A] = ???
+    type Par[A] = ExecutorService => Future[A]
 
+    def unit[A](a : A) : Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+    private case class UnitFuture[A](get : A) extends Future[A] {
+        def isDone = true;
+        def get(timeout: Long, units: TimeUnit) = get
+        def isCancelled = false;
+        def cancel(evenIfRunning:Boolean) : Boolean = false
+    }
+
+    private case class Map2Future[A,B,C](a : Future[A], b : Future[B])(f : (A,B) => C) extends Future[C]{
+        var cache : Option[C] = None
+        def isDone = cache.isDefined
+        def get(timeout: Long, units : TimeUnit) : Option[C] = compute(timeout, units )
+        def isCancelled = a.isCancelled || b.isCancelled
+        def cancel(evenIfRunning:Boolean) : Boolean = a.cancel(evenIfRunning) && b.cancel(evenIfRunning)
+
+        def compute(timeout: Long, units : TimeUnit): Option[C] = {
+            cache match {
+                case Some(c) => Some(c)
+                case None => {
+                    val start = System.currentTimeMillis()
+                    val af = a.get(timeout, units)
+                    val mid = System.currentTimeMillis()
+                    val bf = b.get(mid - start, units)
+                    val ret = f(af,bf)
+                    cache = Some(ret)
+                    cache
+                }
+            }
+
+        }
+    }
     def lazyUnit[A](a : => A) : Par[A] = fork (unit(a))
 
-    def fork[A](a : Par[A]) : Par[A] = ???
+    def fork[A](a : => Par[A]) : Par[A] = es => es.submit(new Callable[A]{def call = a(es).get})
 
-    def map2[A](v1 :  A, v2: A)(f :(A,A) => A) : Par[A] = ???
+    def map2[A,B,C](v1 :  Par[A], v2: Par[B])(f :( A, B) => C) : Par[C] =
+        (es : ExecutorService) => {
+            val v1f = v1(es)
+            val v2f = v2(es)
+            Map2Future(v1f, v2f)(f)
+        }
 
-    def run[A](a : Par[A]) : A = ???
+    def asyncF[A,B](f: A => B) : A => Par[B] =
+        (a : A) => lazyUnit(f(a))
+
+    def run[A](s: ExecutorService)(a : Par[A]) : Future[A] = a(s)
 }
